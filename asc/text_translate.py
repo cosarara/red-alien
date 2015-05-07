71c7e5ba10592e96beacf8fba6f92135232ac0ee
Jaume Delclòs Coll <cosa.rara97@gmail.com>
2015-05-07 19:36:04 +0200

    Remove unused imports

--- asc/text_translate.py
+++ asc/text_translate.py
@@ -16,11 +16,9 @@
 #    You should have received a copy of the GNU General Public License
 #    along with ASC.  If not, see <http://www.gnu.org/licenses/>.
 
-import string
 from string import hexdigits
 import sys
 import os
-import pkgutil
 
 # windows builds are frozen
 if getattr(sys, 'frozen', False):
